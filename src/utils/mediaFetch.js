132fb35a4fc111a30610a58fac12d52958c6d262
immortalWebDev <pgbadgujar007@gmail.com>
2026-01-03 22:33:12 +0530

    Update searchPhotosPix function to include pagination parameters

--- src/utils/mediaFetch.js
+++ src/utils/mediaFetch.js
@@ -1,4 +1,4 @@
-export async function searchPhotosPix(query) {
+export async function searchPhotosPix(query, page = 1, per_page = 4) {
 
   console.log("pixabay req sent..");
   
@@ -6,16 +6,22 @@ export async function searchPhotosPix(query) {
   const params = new URLSearchParams({
     key: API,
     q: query,
+    page,
+    per_page,
+    image_type: "photo",
+    safesearch: "true",
   });
+  //   console.log(params);
 
   const res = await fetch(`https://pixabay.com/api/?${params}`);
 
-  console.log(res.status);
+//   console.log(res.status);
   if (res.status === 429) {
     throw new Error("Too many requests..Try after an hour");
   }
+  if (!res.ok) throw new Error("Pixabay error");
 
   let data = await res.json();
-
+//   console.log(data.hits);
   return data.hits;
 }
